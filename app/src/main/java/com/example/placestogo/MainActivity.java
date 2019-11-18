4ed8bcbcd365089782b683a0bbda0173b7675352
Jasper Helmich <jasper.helmich@gmail.com>
2019-11-18 21:38:29 +0100

    Revert "Dont understand shit"
    This reverts commit 1d95891aa2b123d142082ff9d42706f1bab10d4f.

--- app/src/main/java/com/example/placestogo/MainActivity.java
+++ app/src/main/java/com/example/placestogo/MainActivity.java
@@ -1,21 +1,13 @@
 package com.example.placestogo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-import com.example.placestogo.domain.GoogleApi;
-import com.google.android.libraries.places.api.Places;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        new GoogleApi(getApplicationContext()).getPlaces();
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
