70e29df0e46685434167a39482e3c31e0b8a8b4c
Jasper Helmich <j.helmich@mach3builders.nl>
2020-01-14 21:37:18 +0100

    wip

--- app/src/androidTest/java/com/example/placestogo/PermissionsTest.java
+++ app/src/androidTest/java/com/example/placestogo/PermissionsTest.java
@@ -11,15 +11,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.INTERNET;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class GPSTests {
+public class PermissionsTest {
 
     @Test
     public void gps_isEnabled() {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals(PackageManager.PERMISSION_GRANTED, ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION));
+    }
+
+    @Test
+    public void internet_isEnabled() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals(PackageManager.PERMISSION_GRANTED, ContextCompat.checkSelfPermission(context, INTERNET));
     }
 }
