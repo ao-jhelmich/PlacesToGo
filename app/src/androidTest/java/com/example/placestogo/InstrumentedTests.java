0e515a1ae01c81abb3974b1bbc21c3cad3b779e9
Maarten Franken <maartenfranken4@hotmail.com>
2020-01-14 17:39:51 +0100

    Gps enabled test

--- app/src/androidTest/java/com/example/placestogo/InstrumentedTests.java
+++ app/src/androidTest/java/com/example/placestogo/InstrumentedTests.java
@@ -1,11 +1,14 @@
 package com.example.placestogo;
 
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.room.Room;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.example.placestogo.domain.location.GPS;
+import com.example.placestogo.domain.places.Place;
 import com.example.placestogo.persistence.Visited;
 import com.example.placestogo.persistence.VisitedDao;
 import com.example.placestogo.persistence.VisitedDatabase;
@@ -13,6 +16,7 @@ import com.example.placestogo.persistence.VisitedDatabase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -38,5 +42,22 @@ public class InstrumentedTests {
         visitedDao.insert(visited);
 
         assertTrue(db.checkPlaceVisited(context, placeId));
+    }
+
+    @Test
+    public void gps_isEnabled() {
+        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+
+        Place place = new Place("246OM12690WRY9M2MGHSF0246N80SFH0M2M21MLLSFH04262", "Test","url",0.0,0.0);
+        Intent intent = new Intent().setClassName(context, String.valueOf(CompassActivity.class));
+        intent.putExtra("place", place);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(intent);
+        context = InstrumentationRegistry.getInstrumentation().getContext();
+
+        GPS gps = new GPS(context);
+
+        assertEquals("com.example.placestogo", context.getPackageName());
     }
 }
